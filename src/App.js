6c105f131a87c450aa384929c0c94156899f18ec
cecil <cecilghimire@gmail.com>
2022-09-30 23:16:37 +0545

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Graph from './Components/Graph';
 
 function App() {
   return (
     <div className="App">
        <h1>Expense Tracker</h1>
-      
+       <Graph/>      
     </div>
   );
 }
---
+++ src/Components/Graph.jsx
@@ -0,0 +1,45 @@
+import { Chart ,ArcElement} from 'chart.js';
+import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
+
+Chart.register(ArcElement)
+
+  
+  
+const config={
+  data:{
+    datasets:[
+        {
+
+          data: [133,333,111],
+          backgroundColor:['red','blue','yellow'],
+          hoverOffset:4,
+          borderRadius:30,
+          spacing:15
+        }]
+    } ,
+    options:{
+       cutout:225,
+       plugins: {
+        legend: {
+          position: 'top',
+        },}
+    } 
+}
+
+export default function Graph() {
+  return (
+    <div className='flex justify-content max-w-xs mx-auto'>
+       <div className="item">
+            <div className="chart relative">
+            <Doughnut {...config}/>
+                <h3 className="mb-4 font-bold title">Total</h3>
+                 <span className='block text-3xl text-emerald-400 '>$4000</span>
+            </div>
+            <div className="flex flex-col py-10 gap-4">
+        </div>
+        </div> 
+    </div>
+  )
+}
+ 
